2d7e470e18d0d9df7b9ae73a0f8c08a5cdf53cf7
Mateus Rocca <130199013+MateusRocca@users.noreply.github.com>
2023-09-05 21:48:04 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        console.log('Elemento não encontrado ou seletor inválido')
+    }   
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla'); //'OBS:A declaração const cria uma variável cujo o valor é fixo, ou seja, uma constante somente leitura. Isso não significa que o valor é imutável, apenas que a variável constante não pode ser alterada ou retribuída'. Chama todas as teclas de som.
+
+//CÓDIGO USANDO A ESTRUTURA FOR 'Para'
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
+}
